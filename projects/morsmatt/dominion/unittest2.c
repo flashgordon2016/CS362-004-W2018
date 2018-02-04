a45cb17095160ff626a07a4026bb41c64aa55559
Matthew Morse <morsmatt@oregonstate.edu>
2018-02-03 17:02:55 -0900

    Added unit test file for getCost() and made update to makefile to run new tests

---
+++ projects/morsmatt/dominion/unittest2.c
@@ -0,0 +1,63 @@
+/************************************************************************
+ ## CS362-winter2018-assignment3
+ ## Author: Matthew G. Morse
+ ## ONID: morsatt
+ ## Description: Test unit for the dominion.c getCost() function. (Appears
+   on lines 584-645 of dominion.c)
+**************************************************************************/
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "cards.h" //refactored card functions
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int compare()
+
+void testGetCost() {
+  int i, passed;
+  int distinctCards = 27;
+
+  //Check that getCost() returns nonnegative values for all valid cards
+  passed = 1; //Test is assumed successful until proven otherwise
+  for (int i = 0; i < distinctCards && passed == 1; i++) {
+    if (getCost(i) < 0)
+      passed = 0;
+  }
+  if (passed == 1)
+    printf("_getCost(): PASS negative cost check\n");
+  else
+    printf("_getCost(): FAIL negative cost check\n");
+
+  //Check that getCost() returns invalid value (negative) when called
+  //to find non-existent card
+  passed = 1;
+  if (getCost(-1) < 0 || getCost(30) < 0)
+    printf("_getCost(): PASS invalid card enum check\n");
+  else
+    printf("_getCost(): FAIL invalid card enum check\n");
+
+  //Check that the state of the players in a game is not altered by
+  //calling getCost()
+
+  //Check that the state of the supply decks is not altered by
+  //calling getCost()
+
+  //Check that no card costs more than 8 coins
+  passed = 1; //Test is assumed successful until proven otherwise
+  for (int i = -1; (i < (distinctCards + 1)) && passed == 1; i++) {
+    if (getCost(i) < 0)
+      passed = 0;
+  }
+  if (passed == 1)
+    printf("_getCost(): PASS maximum cost check\n");
+  else
+    printf("_getCost(): FAIL maximum cost check\n");
+}
+
+
+int main(int argc, char *argv[]) {
+  testGetCost();
+  return 0;
+}
