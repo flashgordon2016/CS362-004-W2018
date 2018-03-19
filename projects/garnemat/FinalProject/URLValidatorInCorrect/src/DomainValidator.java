9bca780a8db697684b447f5fc8ee624040fc5ca8
Matt Morse <matt.morse1988@gmail.com>
2018-03-19 09:36:05 -0800

    Bug "fix"
    Bug fix added on line 166.

--- projects/garnemat/FinalProject/URLValidatorInCorrect/src/DomainValidator.java
+++ projects/garnemat/FinalProject/URLValidatorInCorrect/src/DomainValidator.java
@@ -163,7 +163,8 @@ public class DomainValidator implements Serializable {
         }
         String[] groups = domainRegex.match(domain);
         if (groups != null && groups.length > 0) {
-            return !isValidTld(groups[0]);
+            return isValidTld(groups[0]);
+            //return !isValidTld(groups[0]);    <--------------------- "Fixed" bug here
         }
         return allowLocal && hostnameRegex.isValid(domain);
     }
